1defd7d139089fe4e5156031e4dcd1b01acaa6d7
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-21 02:52:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const dbPath = "mongodb://127.0.0.1:27017/booking_app"
 
+const storeRouter = require('./router/storeRouter');
+
 const app = express();
-const port = 3001;
+const port = 3002;
 
+app.set("view engine","ejs")
+app.set("views","views")
 
+app.use(express.urlencoded())
+
+app.use(storeRouter)
 
 
 mongoose.connect(dbPath)
-.then(()=>{
-    app.listen(port,()=>{
-        console.log(`server is running on http://localhost:${port}`);
-        
+    .then(()=>{
+        app.listen(port,()=> console.log(`server is running on http://localhost:${port}`))
     })
-
-})
-.catch(err=>{
-    console.log("mongodb is not conected yrt try again!",err);
-})
+    .catch(err=>{
+        console.log("mongodb is not conected yrt try again!",err);
+    })
 
