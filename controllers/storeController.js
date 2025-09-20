bb9fade03c372de3fc68bdecd3acbbaaa747f48d
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-21 03:14:17 +0500

    added new files

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,24 @@
 exports.getIndex = (req,res)=>{
     res.render('index',{
-        pageTitle: "Homies"
+        pageTitle: "Homies",
+        activePage: "index"
+    })
+}
+exports.getHomes = (req,res)=>{
+    res.render('store/homes',{
+        pageTitle: "Homes",
+        activePage: "homeList"
+    })
+}
+exports.getFav = (req,res)=>{
+    res.render('store/fav',{
+        pageTitle: "Favorite home",
+        activePage: "favorite"
+    })
+}
+exports.getBook = (req,res)=>{
+    res.render('store/book',{
+        pageTitle: "Booking",
+        activePage: "booking"
     })
 }
--- router/storeRouter.js
+++ router/storeRouter.js
@@ -4,6 +4,9 @@ const storeController = require('../controllers/storeController')
 const storeRouter = express.Router()
 
 storeRouter.get('/',storeController.getIndex)
+storeRouter.get('/homeList',storeController.getHomes)
+storeRouter.get('/favorite',storeController.getFav)
+storeRouter.get('/bookings',storeController.getBook)
 
 
 module.exports = storeRouter; 
