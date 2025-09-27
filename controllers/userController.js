aa49ba0125c304b32f3b980f0d4eb61fbd1029ae
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-27 22:28:03 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const homeSchema = require("../model/homes")
+
 exports.getRegistration = (req,res)=>{
     res.render('user/addHome',{
         pageTitle: "Homies",
@@ -5,8 +7,20 @@ exports.getRegistration = (req,res)=>{
     })
 }
 
-exports.addedHome = (req,res)=>{
+exports.addedHome = async (req,res)=>{
+    const home =  await homeSchema.find()
     res.render('user/homeadded',{
+        booking:home,
+        pageTitle: "Homies",
+        activePage: "index"
+    })
+}
+exports.postRegistration = (req,res)=>{
+    console.log(req.body);
+    const {userName,location,price,rating,pic,description} = req.body
+    const home = homeSchema({houseName:userName,location,price,rating,pic,description})
+    home.save()
+    res.render('user/registerhome',{
         pageTitle: "Homies",
         activePage: "index"
     })
