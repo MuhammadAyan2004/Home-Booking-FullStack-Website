2bd1683c63f7a647f1f9cbb08565a702d132ed39
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-27 22:27:59 +0500

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController')
 const userRouter = express.Router()
 
 userRouter.get('/host/addHome',userController.getRegistration)
-userRouter.get('/host/homeAdded',userController.addedHome)
+userRouter.get('/host/homes',userController.addedHome)
+userRouter.post('/host/registration',userController.postRegistration)
 
 module.exports = userRouter
