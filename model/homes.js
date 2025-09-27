92baa1bfe1d84b6ec325824df15946718bdcfd0d
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-27 22:28:01 +0500

    Create homes.js

---
+++ model/homes.js
@@ -0,0 +1,12 @@
+const { default: mongoose } = require("mongoose");
+
+const homeSchema = new mongoose.Schema({
+    houseName:{type:String, required:true},
+    location:{type:String, required:true},
+    price:{type:Number, required:true},
+    rating:{type:String, required:true},
+    pic:String,
+    description:String
+})
+
+module.exports = mongoose.model('userHome',homeSchema)
