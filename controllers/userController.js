b96bb0950acad81caed2c57a514ae485b06041a5
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-27 23:30:58 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,10 +3,20 @@ const homeSchema = require("../model/homes")
 exports.getRegistration = (req,res)=>{
     res.render('user/addHome',{
         pageTitle: "Homies",
-        activePage: "index"
+        activePage: "index",
+        edithome:false
     })
 }
 
+exports.postRegistration = (req,res)=>{
+    const {userName,location,price,rating,pic,description} = req.body
+    const home = homeSchema({houseName:userName,location,price,rating,pic,description})
+    home.save()
+    res.render('user/registerhome',{
+        pageTitle: "Homies",
+        activePage: "index"
+    })
+}
 exports.addedHome = async (req,res)=>{
     const home =  await homeSchema.find()
     res.render('user/homeadded',{
@@ -15,13 +25,32 @@ exports.addedHome = async (req,res)=>{
         activePage: "index"
     })
 }
-exports.postRegistration = (req,res)=>{
-    console.log(req.body);
-    const {userName,location,price,rating,pic,description} = req.body
-    const home = homeSchema({houseName:userName,location,price,rating,pic,description})
-    home.save()
-    res.render('user/registerhome',{
-        pageTitle: "Homies",
-        activePage: "index"
-    })
+
+exports.getEdithome = async (req,res)=>{
+    console.log(req.params.homeId);
+    const homeId = req.params.homeId;
+    const editing = req.query.editing === 'true'
+
+    const home = await homeSchema.findById(homeId)
+        try {
+            res.render('user/addHome',{
+                pageTitle: "Edit Home",
+                activePage: "index",
+                edithome:home,
+                editing,
+            })   
+        } 
+        catch(err){
+            console.log("could not found your home:", err);
+        }
+}
+exports.postEdithome = async (req,res)=>{
+    const {id,userName,location,price,rating,pic,description} = req.body
+    const home = await homeSchema.findByIdAndUpdate(id,{houseName:userName,location,price,rating,pic,description},{new:true})
+        try {
+            res.redirect('/host/homes')   
+        } 
+        catch(err){
+            console.log("could not found your home:", err);
+        }
 }
