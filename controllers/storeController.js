584796201f3384afbe40cbc209ef57699cc9647a
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-30 00:33:27 +0500

    Update storeController.js

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,16 +1,35 @@
+const homeSchema = require("../model/homes")
+
 exports.getIndex = (req,res)=>{
     res.render('index',{
         pageTitle: "Homies",
         activePage: "index"
     })
 }
-exports.getHomes = (req,res)=>{
+exports.getHomes = async (req,res)=>{
+    const homes = await homeSchema.find()
     res.render('store/homes',{
         pageTitle: "Homes",
-        activePage: "homeList"
+        activePage: "homeList",
+        home:homes,
+    })
+}
+exports.getdetails = async (req,res)=>{
+    const homeId = req.params.homeId; 
+    const home = await homeSchema.findById(homeId)
+    res.render('store/homeDetails',{
+        pageTitle: "Home detail",
+        activePage: "homeList",
+        home:home,
     })
 }
 exports.getFav = (req,res)=>{
+    res.render('store/fav',{
+        pageTitle: "Favorite home",
+        activePage: "favorite"
+    })
+}
+exports.postaddFav = (req,res)=>{
     res.render('store/fav',{
         pageTitle: "Favorite home",
         activePage: "favorite"
