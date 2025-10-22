bc4cf7e472deda459f2bbc41f08c42e527a98380
Muhammad AYAN <m101ayan92@gmail.com>
2025-10-22 20:43:41 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const dbPath = "mongodb://127.0.0.1:27017/booking_app"
+const session = require('express-session')
+const mongodb_store = require('connect-mongodb-session')(session)
 
 const storeRouter = require('./router/storeRouter');
 const userRouter = require('./router/userRouter');
 const authRouter = require('./router/authRouter');
 
+const dbPath = "mongodb://127.0.0.1:27017/booking_app"
 const app = express();
 const port = 3002;
 
@@ -13,6 +15,18 @@ app.set("view engine","ejs")
 app.set("views","views")
 
 app.use(express.urlencoded())
+
+const store = new mongodb_store({
+    uri: dbPath,
+    collection:'sessions'
+})
+
+app.use(session({
+    secret:"It's secret",
+    resave:false,
+    saveUninitialized:true,
+    store
+}))
 
 app.use(authRouter)
 
