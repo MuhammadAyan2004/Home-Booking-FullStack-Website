591ad0043f2c05f0f393e41e3e8ee5cad8f50748
Muhammad AYAN <m101ayan92@gmail.com>
2025-10-23 23:55:01 +0500

    Update authRouter.js

--- router/authRouter.js
+++ router/authRouter.js
@@ -4,7 +4,7 @@ const authController = require('../controllers/authController')
 const authRouter = express.Router()
 
 authRouter.get('/login',authController.getLogin)
-// authRouter.post('/login',authController.postLogin)
+authRouter.post('/',authController.postLogin)
 authRouter.get('/signUp',authController.getSignUp)
 // authRouter.post('/signUp',authController.getSignUp)
 
