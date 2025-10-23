b75e06853bbbc2632369b5e0b7e85ebfdb662c04
Muhammad AYAN <m101ayan92@gmail.com>
2025-10-23 23:54:55 +0500

    Update signModel.js

--- model/signModel.js
+++ model/signModel.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose')
 
 const signUpSchema = new mongoose.Schema({
+    firstName:{type:String, required:true},
+    lastName:{type:String, required:true},
+    email:{type:String, required:true},
+    password:{type:String, required:true},
+    accType:{type:String, required:true},
+    favorites:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'userHome'
+    }],
 
 })
 
